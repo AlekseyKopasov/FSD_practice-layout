dab8d92f3eedd15c990cb734aa12734b4e2c6694
Aleksey Kopasov <alesey.k1@gmail.com>
2020-02-05 01:41:14 +0300

    Добавляет сборку

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/* src/app.js */
+
+// Styles
+import 'styles/_app.scss';
+
+// Libraries
+import 'slick-carousel';
+
+// Scripts
+$(document).ready(() => {
+  console.log('Ready!');
+
+  require('scripts/carousel');
+});
